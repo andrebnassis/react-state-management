7f683ab5daa2dd3b1024512c1e98c57246e29a0b
andrebnassis <andrebnassis@gmail.com>
2022-02-03 13:35:06 -0300

    implement request/success/failure pattern

--- src/redux-core/action-creators/ItemListActionCreator.ts
+++ src/redux-core/action-creators/ItemListActionCreator.ts
@@ -4,10 +4,14 @@ import {AppDispatch, RootState} from '../store'
 export const loadItemlist = () => {
     return async (dispatch:AppDispatch, getState:RootState) => {
         
-        const result = await axios.get("http://localhost:9000/todos");
-        if(result.status === 200)
-        {
-            dispatch({type: 'LOAD_DATA', payload:{ itemList: result.data}})    
+        dispatch({type: 'LOAD_DATA_REQUEST'});
+
+        try{
+            const result = await axios.get("http://localhost:9000/todos");
+            dispatch({type: 'LOAD_DATA_SUCCESS', payload:{ itemList: result.data}})    
+        }
+        catch(err){
+            dispatch({type: 'LOAD_DATA_FAILURE', error: err})    
         }
     }
 }
--- src/redux-core/reducers/ItemListReducer.ts
+++ src/redux-core/reducers/ItemListReducer.ts
@@ -1,6 +1,20 @@
 import { ITodoItem } from "../../components/ITodoItem";
 
-type ItemListAction = |{
+type ItemListAction = 
+|{
+    type: 'LOAD_DATA_REQUEST',
+}
+|{
+    type: 'LOAD_DATA_SUCCESS',
+    payload: {
+        itemList: Array<ITodoItem>
+    }
+}
+|{
+    type: 'LOAD_DATA_FAILURE',
+    error: any
+}
+|{
     type: 'LOAD_DATA',
     payload: {
         itemList: Array<ITodoItem>
@@ -32,6 +46,17 @@ const initialState = {
 
 const ItemListReducer = (state:IState = initialState, action: ItemListAction):IState => {
     switch(action.type){
+        case 'LOAD_DATA_REQUEST':
+            console.log('loading...');
+            return {...state};
+            case 'LOAD_DATA_SUCCESS':
+                console.log('success');
+                return {...state, 
+                    itemList: [...action.payload.itemList]
+                }
+            case 'LOAD_DATA_FAILURE':
+                console.log('failure');
+                return {...state, itemList: [], error: action.error}
         case 'LOAD_DATA':
             return {...state, 
                 itemList: [...action.payload.itemList]
